99415cd0e00fd31cffa59d1fcfa35622854d71fe
ikromr04 <ikromr04@gmail.com>
2023-08-24 14:02:39 +0500

    Adds Controller abstract class

---
+++ src/http/controllers/controller.ts
@@ -0,0 +1,26 @@
+import { Response } from 'express';
+import { ControllerInterface } from './controller.interface.js';
+import { injectable } from 'inversify';
+import { StatusCodes } from 'http-status-codes';
+
+@injectable()
+export default abstract class Controller implements ControllerInterface {
+  public send<T>(res: Response, statusCode: number, data: T): void {
+    res
+      .type('application/json')
+      .status(statusCode)
+      .json(data);
+  }
+
+  public ok<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.OK, data);
+  }
+
+  public created<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.CREATED, data);
+  }
+
+  public noContent<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.NO_CONTENT, data);
+  }
+}
