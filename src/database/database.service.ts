bc72e012006b0886f6cd34be1fc85b84423b8593
ikromr04 <ikromr04@gmail.com>
2023-08-25 15:32:35 +0500

    Adds DatabaseService

---
+++ src/database/database.service.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from 'inversify';
+import { DatabaseInterface } from './database.interface.js';
+import { Component } from '../types/component.type.js';
+import { LoggerInterface } from '../logger/logger.interface.js';
+import mongoose from 'mongoose';
+
+@injectable()
+export default class DatabaseService implements DatabaseInterface {
+  constructor(
+    @inject(Component.LoggerInterface) private logger: LoggerInterface,
+  ) {}
+
+  public async connect(uri: string): Promise<void> {
+    this.logger.info('Trying to connect to the database');
+    await mongoose.connect(uri);
+    this.logger.info('Database connection established.');
+  }
+
+  public async disconnect(): Promise<void> {
+    await mongoose.disconnect();
+    this.logger.info('Database connection closed.');
+  }
+}
